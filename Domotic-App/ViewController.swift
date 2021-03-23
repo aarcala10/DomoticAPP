d052af4ecc3348cc8b496b5016bfcb75bdb6ea9f
Adrián Arcalá <aarcala10@gmail.com>
2021-03-23 18:24:34 +0100

    Initial Commit

---
+++ Domotic-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Domotic-App
+//
+//  Created by Adrian Arcalá Ocón on 23/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
